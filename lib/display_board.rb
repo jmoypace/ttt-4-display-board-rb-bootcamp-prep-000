a07d0f8da4241826b3d5a37d4acdc4e07ab06e84
jmoypace <jmoypace@gmail.com>
2018-05-15 00:36:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,15 +4,7 @@ def display_board(board)
   
   puts"   |   |   "
   puts"-----------"
-  puts"   | X |   "
+  puts"   |   |   "
   puts"-----------"
   puts"   |   |   "
  end 
-def display_board(board)  
-  puts"   |   |   "
-  puts"-----------"
-  puts"   | X |   "
-  puts"-----------"
-  puts"   |   |   "
-  
-end
